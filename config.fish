ee5eb7855d92fb02281f8e1081df2142f741a61e
Dave King <dave@davbo.org>
2012-06-10 21:26:19 +0100

    Added virtualenv to my fish prompt

--- config.fish
+++ config.fish
@@ -3,6 +3,9 @@ set -g -x EDITOR vim
 
 set -g -x PIP_DOWNLOAD_CACHE "$HOME/.pip/cache"
 
+# Stop the activate.fish prompt setting an ugly prompt
+set -g -x VIRTUAL_ENV_DISABLE_PROMPT 1
+
 # Prompt {{{
 
 set normal (set_color normal)
@@ -10,6 +13,7 @@ set magenta (set_color magenta)
 set yellow (set_color yellow)
 set green (set_color green)
 set gray (set_color -o black)
+
 
 function fish_prompt
     set_color yellow
@@ -26,7 +30,11 @@ function fish_prompt
     printf '%s' (prompt_pwd)
     set_color normal
 
+    # Line 2
     echo
+    if test $VIRTUAL_ENV
+        printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+    end
     printf '↪ '
     set_color normal
 end
